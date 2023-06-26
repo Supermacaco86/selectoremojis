f83c3a607c669d53d6a059dd93137f327a1db7ef
Martin Figueroa <supermacaco86@gmail.com>
2023-06-26 13:49:06 -0300

    data

---
+++ src/components/emojiPicker/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        symbol: "😀",
+        name: "cara feliz",
+        keywords: "smiling face happy",
+    },
+    {
+        symbol: "😂",
+        name: "super risa",
+        keywords: "carcajada",
+    },
+    {
+        symbol: "😍",
+        name: "cara enamorada",
+        keywords: "love heart happy",
+    },
+    {
+        symbol: "😎",
+        name: "cara gafas",
+        keywords: "canchero ganador",
+    },
+    {
+        symbol: "🤩",
+        name: "cara estrella",
+        keywords: "super feliz happy",
+    },
+    {
+        symbol: "🥳",
+        name: "cara festiva",
+        keywords: "celebracion fiesta festejo",
+    },
+    {
+        symbol: "😭",
+        name: "cara triste",
+        keywords: "llorar unhappy",
+    },
+    {
+        symbol: "😡",
+        name: "cara enojada",
+        keywords: "brona mal dia",
+    },
+    {
+        symbol: "😱",
+        name: "cara sorprendida",
+        keywords: "sorpresa",
+    }
+]
