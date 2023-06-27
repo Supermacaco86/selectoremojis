ca3de9fa267a4b700a2be41115078c88c93783c3
Martin Figueroa <supermacaco86@gmail.com>
2023-06-27 11:09:58 -0300

    focus

--- src/components/emojiPicker/emojiPickerInput.js
+++ src/components/emojiPicker/emojiPickerInput.js
@@ -5,14 +5,14 @@ export default function EmojiPickerInput(){
 
     const refInput = useRef(null);
 
-    // function handleClick(){
-    //     refInput.current.focus()
-    // }
+    function handleClick(){
+        refInput.current.focus()
+    }
 
     return (
         <div>
             <input ref={refInput}/>
-            {/* <button onClick={handleClick}>😀</button> */}
+            <button onClick={handleClick}>😀</button>
             <EmojiPicker/>
         </div>
     )
